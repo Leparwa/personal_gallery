9f2f15e8239997a77b82502e403e508ef15bbedb
leparwa <kevin.leparwa@student.moringaschool.com>
2021-09-06 12:04:24 +0300

    get all photos

--- album/views.py
+++ album/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Image
+from .models import Image, Category, Location
 import datetime as dt
 
 
@@ -9,6 +9,7 @@ def index(request):
     return render(request, 'all_photos/base.html',{"title":"gallery","date":date})
 def all_photos(request):
     all_photos = Image.objects.all()
-    return render(request, 'all_photos/photos.html',{"title":"photos", "photos":all_photos})
+    category = Category.objects.all()
+    return render(request, 'all_photos/photos.html',{"title":"photos", "photos":all_photos, "category":category})
 
 
