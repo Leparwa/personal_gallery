3f41ec773e3c82794bb4eae205e9c1cf98a9a2e7
leparwa <kevin.leparwa@student.moringaschool.com>
2021-09-06 11:10:15 +0300

    all photos layout

--- album/urls.py
+++ album/urls.py
@@ -5,6 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('all-photos', views.all_photos, name='photos'),
-    path('<int:question_id>/results/', views.results, name='results'),
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+
 ]
--- album/views.py
+++ album/views.py
@@ -5,8 +5,6 @@ import datetime as dt
 
 
 def index(request):
-   
- 
     date = dt.date.today()
     return render(request, 'all_photos/base.html',{"title":"gallery","photos": all_photos})
 def all_photos(request):
@@ -14,9 +12,3 @@ def all_photos(request):
     return render(request, 'all_photos/photos.html',{"title":"photos", "photos":all_photos})
 
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
