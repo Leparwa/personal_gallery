a12d6c01620d1025daedf9c5fdfdd84269ea96d4
leparwa <kevin.leparwa@student.moringaschool.com>
2021-09-06 11:11:00 +0300

    all photos layout

--- album/views.py
+++ album/views.py
@@ -6,7 +6,7 @@ import datetime as dt
 
 def index(request):
     date = dt.date.today()
-    return render(request, 'all_photos/base.html',{"title":"gallery","photos": all_photos})
+    return render(request, 'all_photos/base.html',{"title":"gallery","date":date})
 def all_photos(request):
     all_photos = Image.objects.all()
     return render(request, 'all_photos/photos.html',{"title":"photos", "photos":all_photos})
