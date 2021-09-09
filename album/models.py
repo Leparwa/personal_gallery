affbb4f44f978f48c8a60e34089fcd5d2b4ab6bd
leparwa <kevin.leparwa@student.moringaschool.com>
2021-09-09 20:32:38 +0300

    deployment

--- album/models.py
+++ album/models.py
@@ -8,11 +8,33 @@ class Location(models.Model):
     def __str__(self):
         return self.location
 
+    def save(self):
+        self.save()
+
+    @classmethod
+    def update(cls,id):
+        location = cls.objects.update(id = id)
+        return location
+    @classmethod
+    def delete(cls,id):
+        cls.objects.delete(id = id)
+
 class Category(models.Model):
     category = models.CharField(max_length=200)
 
     def __str__(self):
         return self.category
+    
+    def save(self):
+        self.save()
+        
+    @classmethod
+    def update(cls,id):
+        category = cls.objects.update(id = id)
+        return category
+    @classmethod
+    def delete(cls,id):
+        cls.objects.delete(id = id)
 
 
 class Image(models.Model):
@@ -42,6 +64,13 @@ class Image(models.Model):
     def get_image_by_id(cls,id):
         photo = cls.objects.get(id = id)
         return photo
+    @classmethod
+    def delete(cls,id):
+        cls.objects.delete(id = id)
 
+    @classmethod
+    def update(cls,id):
+        photo = cls.objects.update(id = id)
+        return photo
 
     
